264ed9d2c1a535d0d265070800fa3028c4359b75
Lans12345 <olanalans12345@gmail.com>
2024-01-23 14:10:22 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:smarttra_web/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          authDomain: 'smarttra-dc027.firebaseapp.com',
+          apiKey: "AIzaSyDEZZf6w2hYcPapoGiY4dfgwvIoGyrz8zc",
+          appId: "1:352216925523:web:2fc2cdae61a77d37564da6",
+          messagingSenderId: "352216925523",
+          projectId: "smarttra-dc027",
+          storageBucket: "smarttra-dc027.appspot.com"));
   runApp(const MyApp());
 }
 
